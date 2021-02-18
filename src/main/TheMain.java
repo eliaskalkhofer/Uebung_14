81c39343cedd0da89682ec5eb91d6747614e75fd
Elias Kalkhofer <elias.kalkhofer@htl-wels.at>
2021-02-18 10:19:29 +0100

    Added classes and packages

--- src/main/TheMain.java
+++ src/main/TheMain.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,13 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class TheMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/viewcontrol/ViewPhonebook.fxml"));
+        primaryStage.setTitle("Telefonbuch");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
---
+++ src/viewcontrol/ControllerPhonebook.java
@@ -0,0 +1,4 @@
+package viewcontrol;
+
+public class ControllerPhonebook {
+}
